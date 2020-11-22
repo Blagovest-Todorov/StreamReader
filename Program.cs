7e6745895a579b818e880eb7c3870f9ca1758c72
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 17:38:40 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+
+namespace StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader ---we read from a file
+            //The StreamReeaderClass
+            //StreamReaderClass implements methods for reading text lines and sequence of characters
+            //works like Console.Read(), Cosnsole.ReadLine(); but over txt files
+            //stream is like a small river of date packets , but is working over files;
+
+            StreamReader reader = new StreamReader("test.txt");
+            string fileContents = reader.ReadToEnd();
+            Console.WriteLine(fileContents);
+            StreamReader.Close();
+
+            
+        }
+    }
+}
